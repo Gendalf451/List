35e804b32ed155266b962374fdf0163c5e62c971
Gennadii <rgv451@mail.ru>
2023-06-15 19:39:21 +0700

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection<User> user= new ArrayList<>();;
+        for(int i =0; i<10; i++){
+        User user1 = new  User("Вася", "Пупкин");
+        user.add(user1);
+        }
+        System.out.println(user);
+    }
+
+}
